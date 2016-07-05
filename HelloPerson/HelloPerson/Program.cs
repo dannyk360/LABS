d22c7b7aba15e02cfdb622280837c861b26dc0fc
dannyk360 <dannykaplu@gmail.com>
2016-07-05 23:49:37 +0300

    Add files via upload

---
+++ HelloPerson/HelloPerson/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloPerson
+{
+    class Program
+    {
+        public static void PrintN(int k)
+        {
+            for(int i = 0;i < k;i++)
+                Console.Write(" ");
+        }
+        static void Main(string[] args)
+        {
+            int i;
+            string str;
+            Console.WriteLine("Whats your name");
+            str = Console.ReadLine();
+            Console.WriteLine("Hello "+ str);
+            i = int.Parse(Console.ReadLine());
+            for (int j = 0; j < i; j++)
+            {
+                PrintN(j);
+                Console.WriteLine(str);
+            }
+            Console.ReadLine();
+        }
+    }
+}
