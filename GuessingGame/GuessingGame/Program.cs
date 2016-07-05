9eb71e0e1a346d460a1bd966dea3911e581adfe6
dannyk360 <dannykaplu@gmail.com>
2016-07-05 23:48:44 +0300

    Add files via upload

---
+++ GuessingGame/GuessingGame/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessingGame
+{
+    class Program
+    {
+        public static int parseNumber(string str)
+        {
+            int i;
+            if(int.TryParse(str,out i) == false)
+                throw new ArgumentException("not an int");
+            return i;
+        }
+
+        static void Main(string[] args)
+        {
+            string str;
+            int secret = new Random().Next(1, 101),i,j = 0;
+            while (j++ < 7)
+            {
+                str = Console.ReadLine();
+                i = parseNumber(str);
+                if (i < secret)
+                    Console.WriteLine("too small");
+                else if (i > secret)
+                    Console.WriteLine("too big");
+                else {
+                    Console.WriteLine("you right");
+                    break;
+                   }
+            }
+            if (j == 8)
+                Console.WriteLine("you have failed");
+            Console.ReadLine();
+        }
+    }
+}
