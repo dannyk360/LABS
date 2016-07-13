a75c72db945f55af28785920a41020a041941a11
dannyk360 <dannykaplu@gmail.com>
2016-07-13 15:33:09 +0300

    Add files via upload

---
+++ module 10/FileFinder/FileFinder/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileFinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                Console.WriteLine("not enought arguments");
+                Console.ReadLine();
+                return;
+            }
+            try
+            {
+                var namesF = Directory.GetFiles(args[0], "*" + args[1] + "*");
+                var namesD = Directory.GetDirectories(args[0], "*" + args[1] + "*");
+                foreach (var name in namesF)
+                {
+                    Console.WriteLine(name);
+                }
+                foreach (var name in namesD)
+                {
+                    Console.WriteLine(name);
+                }
+            }
+            catch (DirectoryNotFoundException)
+            {
+                Console.WriteLine("directory not found");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ module 10/personnel/Personnel/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Personnel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> strlist = ReadFromTxt();
+            foreach (string str in strlist)
+            {
+                Console.WriteLine(str);
+            }
+
+            Console.ReadLine();
+        }
+
+        public static List<string> ReadFromTxt()
+        {
+            List<string> strList = new List<string>();
+            StreamReader Sr = new StreamReader(@"C:\Users\danny_000\Documents\Visual Studio 2015\Projects\Personnel\Personnel\Names.txt");
+            while(!Sr.EndOfStream)
+                strList.Add(Sr.ReadLine());
+            return strList;
+        }
+    }
+}
