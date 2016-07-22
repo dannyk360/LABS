29c1e425482fb0d50605e870bbdb3900fe3d5083
dannyk360 <dannykaplu@gmail.com>
2016-07-22 16:33:04 +0300

    Add files via upload

---
+++ GenericApp2.2/GenericApp/MultiDictonary.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace GenericApp
+{
+    public class MultiDictionary<K, V> : IMultiDictionary<K, V>, IEnumerable<KeyValuePair<K, IEnumerable<V>>> where K:struct where V:new()
+    {
+        public Dictionary<K, LinkedList<V>> Dictionary { get; }
+        
+        public MultiDictionary()
+        {
+            Dictionary = new Dictionary<K, LinkedList<V>>();
+            Count = 0;
+        }
+        public void Add(K key, V value)
+        {
+            if(typeof(K).GetCustomAttributes(typeof(KeyAttribute),true) == null)
+                throw new Exception("Key must have attribute");
+            if (Dictionary.ContainsKey(key))
+            {
+                Dictionary[key].AddLast(value);
+            }
+            else
+            {
+                Dictionary.Add(key, new LinkedList<V>());
+                Dictionary[key].AddLast(value);
+            }
+            
+            Count++;
+            
+        }
+
+        public void CreateNewValue(K key) 
+        {
+            Add(key, new V());
+        }
+
+        public bool Remove(K key)
+        {
+            
+            int i = Dictionary[key].Count;
+            if (!Dictionary.Remove(key)) return false;
+            
+            Count -= i;
+           
+            
+            return true;
+        }
+
+        public bool Remove(K key, V value)
+        {
+            
+            if (!Dictionary.ContainsKey(key))
+            {
+                
+                return false;
+            }
+            if (!Dictionary[key].Remove(value))
+            {
+                
+                return false;
+            }
+            
+            Count--;
+            
+            return true;
+        }
+
+        public void Clear()
+        {
+            Dictionary.Clear();
+            Count = 0;
+        }
+
+        public bool ContainsKey(K key)
+        {
+            
+            var result = Dictionary.ContainsKey(key);
+            
+            return result;
+        }
+
+        public bool Contains(K key, V value)
+        {
+            
+            if (!Dictionary.ContainsKey(key))
+            {
+                return false;
+            }
+            var res = Dictionary[key].Contains(value);
+           
+            return res;
+        }
+
+        public ICollection<K> Keys { get; }
+        public ICollection<V> Values { get; }
+        public int Count { get; private set; }
+
+        public IEnumerator<KeyValuePair<K, IEnumerable<V>>> GetEnumerator()
+        {
+            var list = Dictionary.Select(item => new KeyValuePair<K, IEnumerable<V>>(item.Key, item.Value)).ToList();
+
+            return list.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
+    public class KeyAttribute : Attribute
+    {
+
+    }
+}
---
+++ GenericApp2.2/GenericApp/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+
+namespace GenericApp
+{
+    class Program
+    {
+        private static void Main()
+        {
+            var multiDictionary = new MultiDictionary<numberWithAttribute, StringBuilder>();
+
+            numberWithAttribute number;
+            number.num = 1;
+
+            
+            multiDictionary.CreateNewValue(number);
+
+            Display(multiDictionary);
+            multiDictionary.Remove(number);
+            Display(multiDictionary);
+            Console.Read();
+
+        }
+
+        private static void Display(MultiDictionary<numberWithAttribute, StringBuilder> multiDictionary)
+        {
+            if (multiDictionary.Count != 0)
+                foreach (var key in multiDictionary)
+                    foreach (var value in key.Value)
+                        Console.WriteLine($"Key: {key.Key.num}, Value: {value}");
+            else
+                Console.WriteLine("The Dictionary has been cleared and is now Empty.");
+        }
+    }
+    [Key]
+    public struct numberWithAttribute
+    {
+        public int num;
+    }
+}
