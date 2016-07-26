640772319a46bad8a3474902436897f812991b6c
Moaid Hathot <moaid.hathot@outlook.com>
2016-07-26 10:39:08 +0300

    2.1

--- HelloPerson/HelloPerson/Program.cs
+++ HelloPerson/HelloPerson/Program.cs
@@ -6,20 +6,31 @@ using System.Threading.Tasks;
 
 namespace HelloPerson
 {
+    //Doesn't handle invalid input.
+
     class Program
     {
         public static void PrintN(int k)
         {
-            for(int i = 0;i < k;i++)
+            //the convention in C# is to always use "{ }" bracets in body of expressions, even for one liners.
+            for (int i = 0; i < k; i++)
+            {
                 Console.Write(" ");
+            }
         }
         static void Main(string[] args)
         {
+            //Unlike in C, in C# you don't have to declare variables at the begining. You can declare them wheneven you want.
             int i;
             string str;
+
             Console.WriteLine("Whats your name");
             str = Console.ReadLine();
-            Console.WriteLine("Hello "+ str);
+
+            //It is better to use string formatting instead of concatenations in such a way.
+            //You can use string.Format, string interpolation (the '$' sign in c# 6) or even the Console.WriteLine overload that accepts formats
+            //Console.WirteLine("Hello", str);
+            Console.WriteLine("Hello " + str);
             i = int.Parse(Console.ReadLine());
             for (int j = 0; j < i; j++)
             {
