13bb3f42c2b1863ec37e37a9355938859dd457c3
Eyal Perry <e.perry.pa@gmail.com>
2016-07-29 18:58:30 +0300

    module 5

--- module 5/ShapeLib/ShapeLib/Class2.cs
+++ module 5/ShapeLib/ShapeLib/Class2.cs
@@ -6,17 +6,43 @@ using System.Threading.Tasks;
 
 namespace ShapeLib
 {
-    public class Rectangle : Shape , IPersist , IComparable
+    public class Rectangle : Shape, IPersist, IComparable
     {
+        //+1 for using readonly :)
         private readonly int _width;
         private readonly int _height;
 
-        public Rectangle(int wid,int heig) : base()
+        /*
+        No input validation for 'width' nor 'height'
+        */
+        public Rectangle(int wid, int heig) : base()
         {
             _width = wid;
             _height = heig;
         }
-        public Rectangle(int wid, int heig,ConsoleColor c) : base(c)
+
+        /*
+        Consider the following DRY implementation:
+
+       public Rectangle(ConsoleColor color,int width, int height):base(color)
+       {
+         Initialize(width,height);
+       }
+
+       public Rectangle(int width, int height):base(color)
+       {
+          Initialize(width,height);
+       }
+
+       private void Initialize(int width, int height)
+       {
+            _width = width;
+           _height = height;
+           _area = _width*_height;
+       }
+
+       */
+        public Rectangle(int wid, int heig, ConsoleColor c) : base(c)
         {
             _width = wid;
             _height = heig;
@@ -32,12 +58,13 @@ namespace ShapeLib
             get { return _height; }
         }
 
-        public override double Area => Width*Height;
+        //Consider computing this at construct time.
+        public override double Area => Width * Height;
 
-        public override void Display() 
+        public override void Display()
         {
-            Console.BackgroundColor = Color;
-            Console.WriteLine("the width is " + Width + " the height is " + Height );
+            Console.BackgroundColor = Color;//Replace this with 'base.Display();'
+            Console.WriteLine("the width is " + Width + " the height is " + Height);
         }
 
         public void Write(StringBuilder sb)
@@ -47,6 +74,12 @@ namespace ShapeLib
 
         public int CompareTo(object obj)
         {
+            /**
+             The compiler exists for your own good.
+             Shutting it up is rarely a good solution to anything.
+
+             Consider using Rectangle as a reference, check for null and if it is indeed a rectangle
+             */
             dynamic shape = obj;
             if (this.Area > shape.Area)
                 return 1;
