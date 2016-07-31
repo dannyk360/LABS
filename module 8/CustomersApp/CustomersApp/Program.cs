8cea4f4ba5d087f828634efa43bf32fc9398ffd7
Moaid Hathot <moaid.hathot@outlook.com>
2016-08-01 00:27:23 +0300

    8.1

--- module 8/CustomersApp/CustomersApp/Program.cs
+++ module 8/CustomersApp/CustomersApp/Program.cs
@@ -12,11 +12,13 @@ namespace CustomersApp
 
         public static bool AToKFilter(Customer customer)
         {
+            //What is this? I can't understand.
             if (string.Compare(customer.Name, "l") == -1)
                 return true;
             return false;
         }
 
+        //didn't use yield or IEnumerable
         public static Collection<Customer> GetCustomers(Collection<Customer> customers, Predicate<Customer> CustomerFilter)
         {
             Collection<Customer> newCustomers = new Collection<Customer>();
@@ -32,9 +34,13 @@ namespace CustomersApp
         {
             Customer[] customerArr = new Customer[10];
             Predicate<Customer> CustomerFilter = AToKFilter;
+
+            //Consider extracting this logic to a different method
             for (int i = 0; i < 5; i++)
             {
                 customerArr[i] = new Customer();
+
+                //ToString() here is redundant.
                 customerArr[i].Name = "danny" + ((2*i)%5).ToString();
                 customerArr[i].ID = (80 * i) % 200;
             }
@@ -60,17 +66,21 @@ namespace CustomersApp
             }
             Console.WriteLine();
             customerArr2 = GetCustomers(new Collection<Customer>(customerArr), delegate (Customer customer){
+                //What about capital letters?
                 if (string.Compare(customer.Name, "k") == 1 && string.Compare(customer.Name, "z") != 1)
                     return true;
                 return false;
             });
             foreach (Customer i in customerArr2)
             {
+                //Why didn't you implement ToString.
                 Console.WriteLine("Name:" + i.Name + " ID:" + i.ID);
             }
             Console.WriteLine();
             Predicate<Customer> myFunc = x => x.ID < 100;
             customerArr2 = GetCustomers(new Collection<Customer>(customerArr), myFunc);
+
+            //Extract this to another method
             foreach (Customer i in customerArr2)
             {
                 Console.WriteLine("Name:" + i.Name + " ID:" + i.ID);
